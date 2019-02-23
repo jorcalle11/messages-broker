9f34853119632d55599e0d8d418f954cedbfe1e5
jorcalle11 <jorcalle11@gmail.com>
2019-02-23 15:29:48 -0500

    message example

---
+++ message.js
@@ -0,0 +1,8 @@
+module.exports = {
+  MessageId: '673c1e3b-0add-40d2-9be5-27b064868fdc',
+  ReceiptHandle:
+    'AQEBHOgaPTnHoJ9vg9hfb/7QSOSzpVrtNVTMTuZy+kJHjjb7NN22P3s/Y8lmezAzEyX1vu3YZXGiamPwaRfrb9KdkpLDo3QL7HtKofJkUwLKtCWMQS2KIYty2xEPvXqPOLsTT2dLee7KC9o6w7sSOQSzhsd40GUd7whrLxoyYJq2yDBjUSrGiMPergb+cLhY9d8fU7+wk+Kq2ea2NVsdoyMkFHASqxUSOfnZcR1GPSlJK2rQRMP3FGvME1nv+bk8lTMjyssQ42xcfVsAeRu1UO74rzEHqFgj/QPE50Gi6XMcCNj4/C3QzvzSJcZF9flUzSePq7jsnnQufLUJQ3faMLDlbb+iw/K0WGcHYD/n4jol4Ms3EtQoIsoOVMh7lt/FmEH+',
+  MD5OfBody: 'ad20f470e1c42d16c564ed21bd35a6ec',
+  Body:
+    '{"firstName":"Raquel","middleName":"Sabryna","lastName":"Beer","email":"Priscilla_Maggio@Zboncak.biz","phone":"947-565-8084","address":"725 Savannah Ports\\nWest Zaria, MN 64632"}'
+};
